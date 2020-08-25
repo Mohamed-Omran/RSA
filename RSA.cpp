ce111a031617ea76a0fd126ec2888eda4163d88a
Mohamed-Omran <kamelomran3@gmail.com>
2020-08-25 18:49:11 +0200

    math done

--- RSA.cpp
+++ RSA.cpp
@@ -8,6 +8,39 @@ using namespace std;
 
 typedef unsigned long long int lli;
 
+
+// gcd of a and b 
+lli gcd(lli a, lli b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+
+lli gcdExtended(lli a, lli b, int* x, int* y)
+{
+    // Base Case  
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+
+    int x1, y1; // To store results of recursive call  
+    int gcd = gcdExtended(b % a, a, &x1, &y1);
+
+    // Update x and y using results of  
+    // recursive call  
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+
+    return gcd;
+}
+
+
+
 lli generate_prime() {
     lli c = rand() % 1000000;
     if (c % 2 == 0)
@@ -86,10 +119,11 @@ bool is_prime(lli p, int iteration)
 int main() {
     srand(time(NULL));
 
-    lli p, q, n, k, e;
-    int j = 5;
+    lli p, q, n, k, e, d;
+    int x, y;
+    int j = 5;  // number of Miller iteration
 
-    //Generating primes p and q randomly
+    //Generating primes p and q and e randomly
     do {
         p = rand() % 1000000; // restricting random number to 999999 to prevent memory overflow
     } while (!is_prime(p,j));
@@ -97,18 +131,53 @@ int main() {
     do {
         q = rand() % 1000000;
     } while (!is_prime(q,j));
-
-    //Computing phi and n
+    
+    //Computing k and n
     n = p * q;
     k = (p - 1) * (q - 1);
+
+    do {
+        e = rand() %k;
+    } while (gcd(e,k)!=1);
+
+
+    lli temp = gcdExtended(k, e, &x, &y);
+    if (y < 0)
+        y += k;
+    
+    d = y;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     cout<< "first prime number p is: " << p << endl;
     cout << "second prime number q is: " << q << endl;
     cout << "n= p * q is: " << n << endl;
-    cout << " k = (p - 1) * (q - 1) is: "<< k << endl;
+    cout << "k = (p - 1) * (q - 1) is: "<< k << endl;
+    cout << "e is: " << e << endl;
+    cout << "gcd(k,e)=: " << gcd(k, e) << endl;
+    cout << x << "  " << y<< endl;
+    //cout << x * k + y * e << endl;
+    //cout << temp << endl;
+
+   cout << "d is " << d << endl;
+   cout << "d*e mod k is: " << (d*e)%k << endl;
 
 
     //cout << Mod_Mul(5, 2, 7)<<endl;
     //cout << Mod_Exp(5, 7, 7) << endl;
     //cout << is_prime(67,5);
-
 }
